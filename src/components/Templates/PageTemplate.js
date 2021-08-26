e93f0979909619908727f341e5763bd93d0041a8
Kim Seohyun <kshjessica@ewhain.net>
2021-08-27 05:51:58 +0900

    feat: palette 추가

--- src/components/Templates/PageTemplate.js
+++ src/components/Templates/PageTemplate.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './PageTemplate.scss';
 
-const PageTemplate = ({ form, children }) => {
+const PageTemplate = ({ form, palette, children }) => {
     return (
         <main className="todo-list-template">
             <div className="title">
                 오늘의 플래너
             </div>
+            <section className="palette-template">
+                {palette}
+            </section>
             <section className="form-wrapper">
                 {form}
             </section>
